593738c15f7024899c99e3665665a7ab478414e3
Ben Wallman <bwallman@andigital.com>
2017-08-08 14:22:38 +0100

    Added basic ajax. Not using a http request library, because I hate myself.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 
+let opts = {
+  "method": "POST",
+  "url": "/login",
+  "data": null
+}
+
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.opts = opts;
+    this.httpRequest = this.httpRequest.bind(this);
+  }
+  
+  httpRequest (options = Object.assign({}, opts)) {
+    let xhr = new XMLHttpRequest()
+    xhr.open(options.method, options.url, true)
+    xhr.send(null)
+    xhr.onreadystatechange = () => {
+      var DONE = 4; // readyState 4 means the request is done.
+      var OK = 200; // status 200 is a successful return.
+      if (xhr.readyState === DONE) {
+        if (xhr.status === OK)
+          console.log(xhr.responseText); // 'This is the returned text.'
+      } else {
+        console.log('Error: ' + xhr.status); // An error occurred during the request.
+      }
+    }
+  }
   render() {
     return (
-     <div style={{textAlign: 'center'}}>
-        <h1>Hello world</h1>
+      <div style={{ textAlign: 'center' }}>
+        <h1 onClick={()=>this.httpRequest()}>Hello world</h1>
       </div>);
   }
 }
--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 9090;
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('dist'));
 
-app.use(express.static(path.resolve('dist/index.html')));
+app.post('/login', (req, res)=>{
+  setTimeout(function(){
+    if(!req.body || !req.body.username || !req.body.password) res.sendStatus(400);
+    else if(req.body.username !== "ben") res.sendStatus(404);
+    else if(req.body.password !== "password") res.sendStatus(401);
+    else res.sendStatus(200);
+  }, 2000);
+});
+
 app.use('/*', express.static(path.resolve('dist/index.html')));
 
 app.listen(PORT, (err) => {
