53a7d0e1498c6b5f75d8fcd35f7145443088e177
Ben Wallman <bwallman@andigital.com>
2017-08-09 10:32:34 +0100

    Finally got unsubscribe to work

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,59 +7,62 @@ let defaultOpts = {
   "data": null
 }
 
+let httpRequest = (options) => {
+  options = Object.assign({}, defaultOpts, options)
+
+  return Rx.Observable.create((observer) => {
+    let xhr = new XMLHttpRequest()
+
+    observer.next(xhr.readyState)
+
+    xhr.open(options.method, options.url, true)
+    xhr.setRequestHeader("Content-Type", "application/json")
+    xhr.send(JSON.stringify(options.data))
+
+    observer.next(xhr.readyState)
+
+    xhr.onreadystatechange = () => {
+      var DONE = 4 // readyState 4 means the request is done.
+      var OK = 200 // status 200 is a successful return.
+      if (xhr.readyState === DONE) {
+        if (xhr.status === OK) {
+          // console.log(xhr.responseText) // 'This is the returned text.'
+          observer.complete()
+        }
+        else {
+          observer.error(xhr.status)
+        }
+      } else {
+        observer.next(xhr.readyState)
+      }
+    }
+  }).subscribe({
+    next: x => console.log('got value ' + x),
+    error: err => console.error('Something wrong occurred: ' + err),
+    complete: () => console.log('Successful response')
+  })
+
+}
+
 export default class App extends React.Component {
   constructor(props) {
     super(props)
-    this.opts = defaultOpts
-    this.httpRequest = this.httpRequest.bind(this)
+    this.httpRequest = httpRequest.bind(this)
+    // this.observables = [];
   }
 
-  httpRequest(options = Object.assign({}, this.opts)) {
-    let httpObservable = Rx.Observable.create((observer) => {
-      let xhr = new XMLHttpRequest()
-      observer.next(xhr.readyState)
-      xhr.open(options.method, options.url, true)
-      xhr.setRequestHeader("Content-Type", "application/json")
-      var obj = {
-        "username": "ben",
-        "password": "password"
-      }
-      xhr.send(JSON.stringify(obj))
-      observer.next(xhr.readyState)
 
-      xhr.onreadystatechange = () => {
-        var DONE = 4 // readyState 4 means the request is done.
-        var OK = 200 // status 200 is a successful return.
-        if (xhr.readyState === DONE) {
-          if (xhr.status === OK) {
-            // console.log(xhr.responseText) // 'This is the returned text.'
-            observer.complete()
-          }
-          else {
-            observer.error(xhr.status)
-          }
-        } else {
-          observer.next(xhr.readyState)
-        }
-      }
-    })
-
-    httpObservable.subscribe({
-      next: x => console.log('got value ' + x),
-
-      error: err => console.error('Something wrong occurred: ' + err),
-      complete: () => console.log('Oh yeah'),
-    })
-
-
-
-    
-
-  }
   render() {
     return (
       <div style={{ textAlign: 'center' }}>
-        <h1 onClick={() => this.httpRequest()}>Hello world</h1>
+        <h1 onClick={() => {
+          this.ongoingRequest = this.httpRequest({ "data": { "username": "ben", "password": "password" } })
+          setTimeout(()=>{
+            console.log(this.ongoingRequest)
+            this.ongoingRequest.unsubscribe()
+            console.log(this.ongoingRequest)
+          }, 1000)
+        }}>Hello world</h1>
       </div>)
   }
 }
